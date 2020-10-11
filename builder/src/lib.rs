f237b5023a189aed23255cc09838cd2230bc95af
zancas <zancas@zingolabs.com>
2020-10-10 20:08:52 -0600

    finished exercise 2

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,10 +1,31 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
+use syn::DeriveInput;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let ast = syn::parse::<DeriveInput>(input.clone()).unwrap();
+    let id = ast.ident;
+    use quote::{format_ident, quote};
+    let builderid = format_ident!("{}Builder", &id);
+    quote!(
+    impl #id {
+        fn builder() -> #builderid {
+            #builderid {
+                executable: None,
+                args: None,
+                env: None,
+                current_dir: None,
+            }
+        }
+    }
 
-    unimplemented!()
+    pub struct #builderid {
+        executable: Option<String>,
+        args: Option<Vec<String>>,
+        env: Option<Vec<String>>,
+        current_dir: Option<String>,
+    })
+    .into()
 }
