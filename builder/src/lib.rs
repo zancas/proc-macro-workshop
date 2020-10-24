74aaa57a0229667000e9e29e75355a1abd22c445
zancas <zancas@zingolabs.com>
2020-10-23 18:15:21 -0600

    cleaned/refactored

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -41,6 +41,7 @@ impl VisitMut for AddOption {
 }
 impl VisitMut for EachElementProcessor {
     fn visit_field_mut(&mut self, node: &mut syn::Field) {
+        self.allfields.push(node.clone());
         if !&node.attrs.is_empty() {
             for attr in &node.attrs {
                 if attr.path.get_ident().unwrap() == "builder" {
@@ -65,45 +66,44 @@ impl VisitMut for EachElementProcessor {
 }
 #[derive(std::fmt::Debug)]
 struct EachElementProcessor {
+    allfields: Vec<syn::Field>,
     eachfields: Vec<(syn::Ident, syn::Lit)>,
 }
 impl EachElementProcessor {
     pub fn new() -> Self {
-        EachElementProcessor { eachfields: vec![] }
+        EachElementProcessor {
+            allfields: vec![],
+            eachfields: vec![],
+        }
     }
 }
 #[proc_macro_derive(Builder, attributes(builder))]
 pub fn xx(input: TokenStream) -> TokenStream {
     // Input section
-    //let tokens = input.clone();
     let mut derive_input_ast = parse_macro_input!(input as DeriveInput);
     let mut ao = AddOption::new();
     let mut eep = EachElementProcessor::new();
     eep.visit_derive_input_mut(&mut derive_input_ast);
     ao.visit_derive_input_mut(&mut derive_input_ast);
-    //dbg!(&eep);
-    dbg!(&eep.eachfields[0].1);
+    dbg!(&eep.allfields[0].ident.as_ref().unwrap().to_string());
     use quote::{format_ident, quote};
     let setter = &mut String::from("");
-    let mandatory_fields = match ao.mandatory {
-        x if !x.is_empty() => {
-            let checker = &mut String::from("if ");
-            for required_field in x {
-                checker.push_str(&format!("self.{}.is_none() || ", required_field));
-                setter.push_str(&format!(
-                    "{required_field}: self.{required_field}.as_ref().unwrap().clone(),\n",
-                    required_field = required_field
-                ));
-            }
-            let length = checker.len() - 3;
-            checker.truncate(length);
-            checker.push_str(r#"{"#);
-            checker.push_str(r#"  return Err(Box::new(String::from("foo"))); "#);
-            checker.push_str(r#"}"#);
-            checker.push_str(r#" else { return Ok(()); }"#);
-            checker.clone()
+    let mandatory_fields = {
+        let checker = &mut String::from("if ");
+        for required_field in ao.mandatory {
+            checker.push_str(&format!("self.{}.is_none() || ", required_field));
+            setter.push_str(&format!(
+                "{required_field}: self.{required_field}.as_ref().unwrap().clone(),\n",
+                required_field = required_field
+            ));
         }
-        _ => "Ok(())".to_string(),
+        let length = checker.len() - 3;
+        checker.truncate(length);
+        checker.push_str(r#"{"#);
+        checker.push_str(r#"  return Err(Box::new(String::from("foo"))); "#);
+        checker.push_str(r#"}"#);
+        checker.push_str(r#" else { return Ok(()); }"#);
+        checker.clone()
     };
     match ao.optional {
         x if !x.is_empty() => {
@@ -120,6 +120,7 @@ pub fn xx(input: TokenStream) -> TokenStream {
     let settertokens: proc_macro2::TokenStream = setter.parse().unwrap();
     let id = derive_input_ast.ident;
     let builderid = format_ident!("{}Builder", &id);
+    //dbg!(&builderid);
     let methods = quote!(
     fn executable(&mut self, executable: String) -> &mut Self {
         self.executable = Some(executable);
